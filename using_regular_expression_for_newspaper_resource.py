3f393c9703baeb89578d84362771e242be52928e
Ryuichi Tachibana <ryuu0614@gmail.com>
2014-11-08 18:23:08 +0900

    改行が入るファイルと入らないファイルを作成しています

--- using_regular_expression_for_newspaper_resource.py
+++ using_regular_expression_for_newspaper_resource.py
@@ -9,8 +9,17 @@ def main(read_file,write_file):
         line = line.strip()
         if re.search('＼Ｓ１＼', line):
             wf.write("\n")
-        elif re.search('^＼Ｔ２＼', line):
-        	wf.write("%s\n" % line)
+        elif re.search('^＼Ｔ２＼　(.+)', line):
+        	#もうちょっとうまい書き方があると思う
+        	wf.write("%s\n" % (re.search('^＼Ｔ２＼　(.+)', line).group(1)))
+        elif re.search('^＼Ｔ２＼(.+)', line):
+        	wf.write("%s\n" % (re.search('^＼Ｔ２＼(.+)', line).group(1)))
+    wf.close()
+    wf = open("mai98a_for_iikaehasseiritu.txt","w")
+    for line in open(write_file,"r"):
+        line = line.strip()
+        if re.search('(.+。).', line):
+            wf.write("%s\n" % (re.search('(.+。).', line).group(1)))
     wf.close()
 
 if __name__ == "__main__":
