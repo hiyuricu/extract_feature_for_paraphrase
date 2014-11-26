b6818cb3a178302fb03f63b3d039a5f19750dd3a
ryu <tachibanaryuichi@ryu-no-air.lan>
2014-11-26 09:47:44 +0900

    単語間の係り受けの回数を示す辞書を作成します。辞書の要素には辞書が入ります。

--- skew_divergence.py
+++ skew_divergence.py
@@ -2,13 +2,14 @@
 # -*- coding:utf-8 -*-
 
 import sys
+from collections import defaultdict
 
-def skew_divergence(read_file):
+def skew_divergence(read_file, X_list, Y_list):
 
-    dependency_word_dic = {}
+    dependency_word_dic = defaultdict(dict)
     string = ""
     string_list = []
-    line_features_list = []
+    line_features_temp_list = []
     phrase_temp_dic = {}
 
     for line in open(read_file, "r"):
@@ -18,28 +19,49 @@ def skew_divergence(read_file):
             pass
 
         elif line[0:2] == "* ":
-            if string != "" and line_features_list != []:
-                phrase_temp_dic[string] = [line_features_list[1], line_features_list[2][0:-1]]
+            if string != "" and line_features_temp_list != []:
+                phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
                 string_list.append(string)
                 string = ""
-            line_features_list = line.split(" ")
+            line_features_temp_list = line.split(" ")
 
         elif line[0:3] == "EOS":
-            if string != "" and line_features_list != []:
-                phrase_temp_dic[string] = [line_features_list[1], line_features_list[2][0:-1]]
+            if string != "" and line_features_temp_list != []:
+                phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
                 string_list.append(string)
                 for string in string_list:
-                    if "受賞者" in string:
-                        for key, value_list in phrase_temp_dic.items():
-                            if phrase_temp_dic[string][1] == value_list[0]:
-                                print key
+                    for X in open(X_list, "r"):
+                        if X in string:
+                            for key, value_list in phrase_temp_dic.items():
+                                if phrase_temp_dic[string][1] == value_list[0]:
+                                    if key in dependency_word_dic[X]:
+                                        dependency_word_dic[X][key] += 1
+                                    else:
+                                        dependency_word_dic[X][key] = 1
+
+                    for Y in open(Y_list, "r"):
+                        if Y in string:
+                            for key, value_list in phrase_temp_dic.items():
+                                if phrase_temp_dic[string][1] == value_list[0]:
+                                    if key in dependency_word_dic[Y]:
+                                        dependency_word_dic[Y][key] += 1
+                                    else:
+                                        dependency_word_dic[Y][key] = 1
             phrase_temp_dic = {}
             string_list = []
             string = ""
-            line_features_list = []
+            line_features_temp_list = []
 
         else:
             string = string + line.split("\t")[0]
 
+    return dependency_word_dic
+
 if __name__ == "__main__":
-    skew_divergence(sys.argv[1])
+    for k,dic in skew_divergence(sys.argv[1], sys.argv[2], sys.argv[3]).items():
+        print k
+        for k2,v in dic.items():
+            print k2,v
+
+
+
