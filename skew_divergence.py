bd5d6c57f75ed75ca02302cdbe9ad967953a612f
ryu <tachibanaryuichi@ryu-no-MacBook-Air.local>
2014-11-26 14:26:22 +0900

    skew_divergenceをprintさせるようにしました(バグあるかも)

--- skew_divergence.py
+++ skew_divergence.py
@@ -3,8 +3,9 @@
 
 import sys
 from collections import defaultdict
+import math
 
-def skew_divergence(read_file, X_list, Y_list):
+def extract_skew_divergence(read_file, csv_file):
 
     dependency_word_dic = defaultdict(dict)
     string = ""
@@ -12,25 +13,26 @@ def skew_divergence(read_file, X_list, Y_list):
     line_features_temp_list = []
     phrase_temp_dic = {}
 
-    for line in open(read_file, "r"):
+    for line in open(csv_file, "r"):
+        ans, X, Y = line.strip().split(',')
+        for line in open(read_file, "r"):
 
-        line = line.strip()
-        if line[0:7] == "* 0 -1D":
-            pass
+            line = line.strip()
+            if line[0:7] == "* 0 -1D":
+                pass
 
-        elif line[0:2] == "* ":
-            if string != "" and line_features_temp_list != []:
-                phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
-                string_list.append(string)
-                string = ""
-            line_features_temp_list = line.split(" ")
+            elif line[0:2] == "* ":
+                if string != "" and line_features_temp_list != []:
+                    phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
+                    string_list.append(string)
+                    string = ""
+                line_features_temp_list = line.split(" ")
 
-        elif line[0:3] == "EOS":
-            if string != "" and line_features_temp_list != []:
-                phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
-                string_list.append(string)
-                for string in string_list:
-                    for X in open(X_list, "r"):
+            elif line[0:3] == "EOS":
+                if string != "" and line_features_temp_list != []:
+                    phrase_temp_dic[string] = [line_features_temp_list[1], line_features_temp_list[2][0:-1]]
+                    string_list.append(string)
+                    for string in string_list:
                         if X in string:
                             for key, value_list in phrase_temp_dic.items():
                                 if phrase_temp_dic[string][1] == value_list[0]:
@@ -39,7 +41,11 @@ def skew_divergence(read_file, X_list, Y_list):
                                     else:
                                         dependency_word_dic[X][key] = 1
 
-                    for Y in open(Y_list, "r"):
+                                    if "all" in dependency_word_dic[X]:
+                                        dependency_word_dic[X]["all"] += 1
+                                    else:
+                                        dependency_word_dic[X]["all"] = 1
+
                         if Y in string:
                             for key, value_list in phrase_temp_dic.items():
                                 if phrase_temp_dic[string][1] == value_list[0]:
@@ -47,21 +53,41 @@ def skew_divergence(read_file, X_list, Y_list):
                                         dependency_word_dic[Y][key] += 1
                                     else:
                                         dependency_word_dic[Y][key] = 1
-            phrase_temp_dic = {}
-            string_list = []
-            string = ""
-            line_features_temp_list = []
 
-        else:
-            string = string + line.split("\t")[0]
+                                    if "all" in dependency_word_dic[Y]:
+                                        dependency_word_dic[Y]["all"] += 1
+                                    else:
+                                        dependency_word_dic[Y]["all"] = 1
 
-    return dependency_word_dic
+                phrase_temp_dic = {}
+                string_list = []
+                string = ""
+                line_features_temp_list = []
+
+            else:
+                string = string + line.split("\t")[0]
+
+        #dependency_word_dicは要素に辞書を持つdefaultdictです
+        return dependency_word_dic
 
 if __name__ == "__main__":
-    for k,dic in skew_divergence(sys.argv[1], sys.argv[2], sys.argv[3]).items():
-        print k
-        for k2,v in dic.items():
-            print k2,v
+    dependency_word_defaultdict_dic = extract_skew_divergence(sys.argv[1], sys.argv[2])
+    for line in open(sys.argv[2], "r"):
+        ans, X, Y = line.strip().split(',')
+        all_skew_divergence = 0
+        for dependency_word, dependency_word_value in dependency_word_defaultdict_dic[X].items():
+            if dependency_word in dependency_word_defaultdict_dic[Y]:
+                #ここでP(i)とQ(i)を計算する
+                Pi = float(dependency_word_defaultdict_dic[X][dependency_word]) / float(dependency_word_defaultdict_dic[X]["all"])
+                Qi = float(dependency_word_defaultdict_dic[Y][dependency_word]) / float(dependency_word_defaultdict_dic[Y]["all"])
+                if Qi > 0:
+                    one_of_skew_divergence = Pi * math.log(Pi / (0.99 * Qi + 0.01 * Pi))
+                    all_skew_divergence += one_of_skew_divergence
+        print all_skew_divergence
+
+
+
+
 
 
 
