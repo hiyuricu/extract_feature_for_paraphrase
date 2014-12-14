91794a7a37fa8637d0c348066192d1be275c5622
ryu <tachibanaryuichi@ryu-no-air.lan>
2014-12-15 01:24:50 +0900

    pickleとかを使わずに素性をprintするようにしてあります

--- convertSingleCol.py
+++ convertSingleCol.py
@@ -12,10 +12,12 @@ ids = 1000
 for line in open(sys.argv[1]):
   line = line.strip()
   if line in id_of.keys():
-    sys.stdout.write('%s:1\n' % (id_of[line]))
+    # sys.stdout.write('%s:1\n' % (id_of[line]))
+    print '%s:1' % (id_of[line])
   else:
     ids += 1
     id_of[line] = ids
-    sys.stdout.write('%s:1\n' % (id_of[line]))
+    # sys.stdout.write('%s:1\n' % (id_of[line]))
+    print '%s:1' % (id_of[line])
 
 # pickle.dump(id_of, open(sys.argv[2], 'w'))
