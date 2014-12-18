dded7d7925f3e1c31a71ba0e8c352d67060f24a0
ryu <tachibanaryuichi@ryu-no-air.lan>
2014-12-18 10:47:29 +0900

    バグを修正しました

--- extract_synonym.py
+++ extract_synonym.py
@@ -7,8 +7,8 @@ import sys, string, re, MeCab
 abbreviation_dic = defaultdict(lambda: 0)
 
 #入力として一文(。がくると改行されている文)一行となっているテキストが引数にあると想定している
-def main(read_file):
-    output_abbreviation(read_file)
+def main():
+    output_abbreviation(sys.argv[1])
     for key, value in sentence_wakati().items():
         print "%s,%s" % (key, value)
 
@@ -35,22 +35,28 @@ def sentence_wakati():
         temp_list = line.strip().split()
         #完全文候補の文が空でないかどうか見ている
         if len(temp_list) == 2:
-            node = tagger.tagger.parseToNode(temp_list[0])
+            # print "----------------------------------------------------------------------------------------"
+            # print line
+            node = tagger.parseToNode(temp_list[0])
             while node:
                 tango = "%s,%s" % (node.surface, node.feature.split(",")[0])
                 tango_list.append(tango)
                 node = node.next
             tango_list.reverse()
             for tango_comma in tango_list[1:-1]:
-                if tango_comma.strip().split(",")[1] != "名詞":
+                # print tango_comma
+                if tango_comma.strip().split(",")[0] == "」":
+                    pass
+                elif tango_comma.strip().split(",")[1] != "名詞" and tango_comma.strip().split(",")[1] != "接頭詞":
                     break
                 else:
-                    perfect_sentence = perfect_sentence + tango_comma.strip().split(",")[0]
+                    perfect_sentence = tango_comma.strip().split(",")[0] + perfect_sentence
         if perfect_sentence != "":
             key_string = "%s,%s" % (perfect_sentence, temp_list[1])
+            # print key_string
             abbreviation_dic[key_string] += 1
 
     return abbreviation_dic
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
