21e3d9ac49f82e1147d6c5b08f080fa6a0d7979b
ryu <tachibanaryuichi@ryu-no-air.lan>
2014-12-20 00:11:00 +0900

    pickleでデータを保存するようにしました

--- extract_wikipedia_html.py
+++ extract_wikipedia_html.py
@@ -1,58 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urllib2, time, sys, jctconv, os.path
+import urllib2, time, sys, jctconv, pickle
 
 #wikipediaのhtmlをクロールします
-f1 = open("error_url", "a")
 i = 0
+f = open("synonym_dict.txt")
+f2 = open("synonym_dict2.txt", "w")
+synonym_dict = pickle.load(f)
 
 for abb_pair_list in open(sys.argv[1], "r"):
     i += 1
     #入力するcsvファイルによってtarget_titleが何番目のフィールドを参照するか変わるので毎回添字をかえる必要がある
-    target_title = abb_pair_list.strip().split(",")[0].decode("utf-8")
-    target_title2 = jctconv.z2h(target_title, kana=False, digit=True, ascii=True)
-    target_title3 = target_title2.encode("utf-8")
-    write_text = "wikipedia_html_synonym/%s.txt" % target_title
-    if not os.path.exists(write_text):
+    target_title_decode = abb_pair_list.strip().split(",")[0].decode("utf-8")
+    target_title_encode = jctconv.z2h(target_title_decode, kana=False, digit=True, ascii=True).encode("utf-8")
+    if not target_title_encode in synonym_dict:
         print i
-        f = open(write_text, "w")
-        url_text = "http://ja.wikipedia.org/wiki/%s" % target_title3
+        url_text = "http://ja.wikipedia.org/wiki/%s" % target_title_encode
         try:
-            url = urllib2.urlopen(url_text).read()
+            url_html = urllib2.urlopen(url_text).read()
+            synonym_dict[target_title_encode] = url_html
             time.sleep(10.0)
         except:
-            f1.write("%s\n") % url_text
-            continue
-        f.write(url)
-        f.close()
-
-f1.close()
-
-
-f2 = open("error_url2", "a")
-i = 0
-
-for abb_pair_list in open(sys.argv[1], "r"):
-    i += 1
-    #入力するcsvファイルによってtarget_titleが何番目のフィールドを参照するか変わるので毎回添字をかえる必要がある
-    target_title = abb_pair_list.strip().split(",")[1].decode("utf-8")
-    target_title2 = jctconv.z2h(target_title, kana=False, digit=True, ascii=True)
-    target_title3 = target_title2.encode("utf-8")
-    write_text = "wikipedia_html_synonym/%s.txt" % target_title
-    if not os.path.exists(write_text):
-        print i
-        f = open(write_text, "w")
-        url_text = "http://ja.wikipedia.org/wiki/%s" % target_title3
-        try:
-            url = urllib2.urlopen(url_text).read()
             time.sleep(10.0)
-        except:
-            f1.write("%s\n") % url_text
             continue
-        f.write(url)
-        f.close()
 
+pickle.dump(synonym_dict, f2)
+f.close()
 f2.close()
-
-
