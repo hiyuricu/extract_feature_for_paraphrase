955e6d795f12ad9d71ccb5b6b71a3b711595ff3d
ryu <tachibanaryuichi@ryu-no-air.lan>
2014-12-20 13:43:58 +0900

    プリントを少し変えました

--- extract_wikipedia_html.py
+++ extract_wikipedia_html.py
@@ -11,17 +11,20 @@ synonym_dict = pickle.load(f)
 
 for abb_pair_list in open(sys.argv[1], "r"):
     i += 1
+    print i
     #入力するcsvファイルによってtarget_titleが何番目のフィールドを参照するか変わるので毎回添字をかえる必要がある
-    target_title_decode = abb_pair_list.strip().split(",")[0].decode("utf-8")
+    target_title_decode = abb_pair_list.strip().split(",")[1].decode("utf-8")
     target_title_encode = jctconv.z2h(target_title_decode, kana=False, digit=True, ascii=True).encode("utf-8")
     if not target_title_encode in synonym_dict:
-        print i
+        print "new_key"
         url_text = "http://ja.wikipedia.org/wiki/%s" % target_title_encode
         try:
             url_html = urllib2.urlopen(url_text).read()
             synonym_dict[target_title_encode] = url_html
+            print "url_succeed"
             time.sleep(10.0)
         except:
+            synonym_dict[target_title_encode] = ""
             time.sleep(10.0)
             continue
 
